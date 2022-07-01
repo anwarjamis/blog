59ee012f8e7c30cad93248574606ce3346025bbf
Anwar Jamis <anwarjamis@gmail.com>
2022-07-01 19:46:41 -0400

    posts controller done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
+  before_Action :set_post, only: %i[show edit update destroy]
 
   def index
     @posts = Post.all
@@ -9,25 +10,41 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
 
   def post_params
+    params.require(:post).permit(:title, :subtitle, :content, :read_time)
   end
 
   def set_post
+    @post = Post.find(params[:id])
   end
 end
